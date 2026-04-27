6c4760e7fd674158626649213c84c3648c4c5427
Vextarhq <vextarhq@gmail.com>
2026-04-27 17:44:45 -0300

    Create page.js

---
+++ app/app/page.js
@@ -0,0 +1,15 @@
+export default function AppPage() {
+  return (
+    <div style={{
+      minHeight: '100vh',
+      background: '#060810',
+      color: '#e8edf2',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      fontFamily: 'monospace'
+    }}>
+      <h1>Vextar App</h1>
+    </div>
+  )
+}
