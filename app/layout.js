72ced63a6191da52f53f19067eb574b81d77a218
Vextarhq <vextarhq@gmail.com>
2026-05-06 10:45:47 -0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -7,7 +7,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider afterSignInUrl="/chat" afterSignUpUrl="/chat">
       <html lang="en">
         <body>{children}</body>
       </html>
