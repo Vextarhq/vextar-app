41123b96a0ab9b0511c473f90b7efe71e15fec23
Vextarhq <vextarhq@gmail.com>
2026-05-06 10:50:42 -0300

    Update page.js

--- app/app/page.js
+++ app/app/page.js
@@ -1,15 +1,5 @@
+import { redirect } from 'next/navigation'
+
 export default function AppPage() {
-  return (
-    <div style={{
-      minHeight: '100vh',
-      background: '#060810',
-      color: '#e8edf2',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      fontFamily: 'monospace'
-    }}>
-      <h1>Vextar App</h1>
-    </div>
-  )
+  redirect('/chat')
 }
