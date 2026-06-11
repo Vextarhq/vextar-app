62554320c535e7312b917c2b97d1627d15c927f8
Vextarhq <vextarhq@gmail.com>
2026-06-11 10:21:26 -0300

    Update route.js

--- app/api/manage-subscription/route.js
+++ app/api/manage-subscription/route.js
@@ -8,7 +8,7 @@ export async function POST(request) {
   }
 
   const supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.SUPABASE_URL,
     process.env.SUPABASE_SERVICE_ROLE_KEY
   )
 
