ff1e5a8a7c59fd97149fb593c3e3f695c680e874
Vextarhq <vextarhq@gmail.com>
2026-07-02 11:09:29 -0300

    Update page.js

--- app/chat/page.js
+++ app/chat/page.js
@@ -59,7 +59,7 @@ export default function Landing() {
     return () => document.removeEventListener('mousemove', move)
   }, [])
 
-  const goToLogin = () => router.push('/login')
+  const goToLogin = () => router.push('/chat')
   const goToPricing = () => router.push('/pricing')
 
   const faqs = [
